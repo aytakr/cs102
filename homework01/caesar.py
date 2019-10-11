936a8e56fa65bf989b2de4e7c2ee8e9aec280d41
aytakr <aytakr@mail.ru>
2019-10-11 20:55:40 +0300

    .

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,7 +1,5 @@
 def encrypt_caesar(plaintext):
     """
-    Encrypts plaintext using a Caesar cipher.
-
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -11,14 +9,20 @@ def encrypt_caesar(plaintext):
     >>> encrypt_caesar("")
     ''
     """
-    # PUT YOUR CODE HERE
+    ciphertext = ""
+    for i in range(0, len(plaintext)):
+        if ord("A") <= ord(plaintext[i]) <= ord("Z"):
+            a = ord("A")
+            ciphertext += chr((ord(plaintext[i]) + 3 - a) % 26 + a)
+        elif ord("a") <= ord(plaintext[i]) <= ord("z"):
+            a = ord("a")
+            ciphertext += chr((ord(plaintext[i]) + 3 - a) % 26 + a)
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
-
 
 def decrypt_caesar(ciphertext):
     """
-    Decrypts a ciphertext using a Caesar cipher.
-
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -28,5 +32,14 @@ def decrypt_caesar(ciphertext):
     >>> decrypt_caesar("")
     ''
     """
-    # PUT YOUR CODE HERE
+    plaintext = ""
+    for i in range(0, len(ciphertext)):
+        if ord("A") <= ord(ciphertext[i]) <= ord("Z"):
+            a = ord("A")
+            plaintext += chr((ord(ciphertext[i]) - 3 - a) % 26 + a)
+        elif ord("a") <= ord(ciphertext[i]) <= ord("z"):
+            a = ord("a")
+            plaintext += chr((ord(ciphertext[i]) - 3 - a) % 26 + a)
+        else:
+            plaintext += ciphertext[i]
     return plaintext
